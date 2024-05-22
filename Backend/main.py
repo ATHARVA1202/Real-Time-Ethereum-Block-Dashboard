68b589809cd7fc515f8bbe677f55fe154b910341
ATHARVA1202 <92789951+ATHARVA1202@users.noreply.github.com>
2024-05-23 00:35:17 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,39 +1,3 @@
-# from fastapi import FastAPI
-# from app.routes import router
-
-# app = FastAPI()
-
-# app.include_router(router)
-
-# # Run the app using: uvicorn main:app --reload
-
-
-# ------------------- NEW (WORKING)---------------------
-
-# from fastapi import FastAPI, BackgroundTasks
-# import uvicorn
-# from app.routes import router
-# from app.routes import continuous_fetch_and_store
-
-# app = FastAPI()
-
-# app.include_router(router)
-
-# @app.on_event("startup")
-# async def startup_event():
-#     # Start the background task
-#     background_tasks = BackgroundTasks()
-#     background_tasks.add_task(continuous_fetch_and_store)
-#     await continuous_fetch_and_store()
-
-# if __name__ == "__main__":
-#     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
-
-
-
-
-#----------------------- NEW ---------------------
-
 from fastapi import FastAPI
 import uvicorn
 import asyncio
@@ -51,5 +15,3 @@ async def startup_event():
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
-
