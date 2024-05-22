1e02746e516f41d8b1ac7e8a24e104d982a95ec4
ATHARVA1202 <92789951+ATHARVA1202@users.noreply.github.com>
2024-05-22 17:04:33 +0530

    Delete Backend directory

--- Backend/app/database.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from .config import settings
-
-client = AsyncIOMotorClient(settings.MONGODB_URI)
-db = client[settings.DB_NAME]
-collection = db[settings.COLLECTION_NAME]
